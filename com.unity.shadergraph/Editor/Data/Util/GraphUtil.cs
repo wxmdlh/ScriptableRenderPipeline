cc78857da3b92e6727507ff5aec53d92672c7bdf
Tristan Genevet <tristan@unity3d.com>
2019-03-12 16:18:26 +0100

    some stuff

--- com.unity.shadergraph/Editor/Data/Util/GraphUtil.cs
+++ com.unity.shadergraph/Editor/Data/Util/GraphUtil.cs
@@ -1526,17 +1526,66 @@ namespace UnityEditor.ShaderGraph
     //Small methods usable by the VFX since they are public
     public static class GraphUtilForVFX
     {
-        public struct Graph
+        public class Graph
         {
             internal GraphData graphData;
             internal List<MaterialSlot> slots;
+
+            internal struct Function
+            {
+                internal List<AbstractMaterialNode> nodes;
+                internal ShaderGraphRequirements requirements;
+            }
+
+            internal struct Pass
+            {
+                internal Function vertex;
+                internal Function pixel;
+            }
+
+
+
+            internal class PassName
+            {
+                public const int GBuffer = 0;
+                public const int ShadowCaster = 1;
+                public const int Depth = 2;
+                public const int Count = 3;
+            }
+
+            internal Pass[] passes = new Pass[PassName.Count];
+
+            internal class PassInfo
+            {
+                public PassInfo(FunctionInfo pixel,FunctionInfo vertex)
+                {
+                    this.pixel = pixel;
+                    this.vertex = vertex;
+                }
+                public readonly FunctionInfo pixel;
+                public readonly FunctionInfo vertex;
+            }
+            internal class FunctionInfo
+            {
+                public FunctionInfo(List<int> activeSlots)
+                {
+                    this.activeSlots = activeSlots;
+                }
+                public readonly List<int> activeSlots;
+            }
+
+            internal readonly static PassInfo[] passInfos = new PassInfo[]
+                {
+                //GBuffer
+                new PassInfo(new FunctionInfo(Enumerable.Range(1, 31).ToList()),new FunctionInfo(Enumerable.Range(0,1).ToList())) // hardcoded pbr pixel slots.
+                };
         }
 
         public static Graph LoadShaderGraph(string shaderFilePath)
         {
             var textGraph = File.ReadAllText(shaderFilePath, Encoding.UTF8);
 
-            Graph graph;
+            Graph graph = new Graph();
             graph.graphData = JsonUtility.FromJson<GraphData>(textGraph);
             graph.graphData.OnEnable();
             graph.graphData.ValidateGraph();
@@ -1549,8 +1598,47 @@ namespace UnityEditor.ShaderGraph
                 else
                     graph.slots.AddRange(activeNode.GetOutputSlots<MaterialSlot>());
             }
+
+
+            graph.passes[Graph.PassName.GBuffer].pixel.nodes = ListPool<AbstractMaterialNode>.Get();
+            NodeUtils.DepthFirstCollectNodesFromNode(graph.passes[Graph.PassName.GBuffer].pixel.nodes, ((AbstractMaterialNode)graph.graphData.outputNode), NodeUtils.IncludeSelf.Include, Graph.passInfos[Graph.PassName.GBuffer].pixel.activeSlots);
+            graph.passes[Graph.PassName.GBuffer].vertex.nodes = ListPool<AbstractMaterialNode>.Get();
+            NodeUtils.DepthFirstCollectNodesFromNode(graph.passes[Graph.PassName.GBuffer].vertex.nodes, ((AbstractMaterialNode)graph.graphData.outputNode), NodeUtils.IncludeSelf.Include, Graph.passInfos[Graph.PassName.GBuffer].vertex.activeSlots);
+
+            graph.passes[Graph.PassName.GBuffer].pixel.requirements = ShaderGraphRequirements.FromNodes(graph.passes[Graph.PassName.GBuffer].pixel.nodes, ShaderStageCapability.Fragment, false);
+            graph.passes[Graph.PassName.GBuffer].vertex.requirements = ShaderGraphRequirements.FromNodes(graph.passes[Graph.PassName.GBuffer].vertex.nodes, ShaderStageCapability.Vertex, false);
+
             return graph;
         }
+
+
+        public static string GenerateMeshAttributesStruct(Graph shaderGraph)
+        {
+            var requirements = shaderGraph.passes[Graph.PassName.GBuffer].vertex.requirements;
+            var vertexSlots = new ShaderStringBuilder();
+
+            vertexSlots.AppendLine("struct AttributeMesh");
+            vertexSlots.AppendLine("{");
+            vertexSlots.IncreaseIndent();
+            vertexSlots.AppendLine("float3 posOS : POSITION");
+            if(requirements.requiresNormal != NeededCoordinateSpace.None)
+                vertexSlots.AppendLine("float4 normalOS : NORMAL");
+            if (requirements.requiresTangent != NeededCoordinateSpace.None)
+                vertexSlots.AppendLine("float4 tangentOS : TANGENT");
+            for(int i = 0; i < 4; ++i)
+            {
+                if( shaderGraph.passes[Graph.PassName.GBuffer].vertex.requirements.requiresMeshUVs.Contains((UVChannel)i))
+                    vertexSlots.AppendLine(string.Format("float4 uv{0} : TEXCOORD{0}",i));
+            }
+            if (requirements.requiresVertexColor)
+                vertexSlots.AppendLine("float4 color : COLOR");
+            vertexSlots.DecreaseIndent();
+            vertexSlots.AppendLine("}");
+
+            return vertexSlots.ToString();
+        }
+
+
 
         public static string GenerateSurfaceDescriptionStruct(Graph shaderGraph)
         {
@@ -1568,7 +1656,7 @@ namespace UnityEditor.ShaderGraph
             var graph = shaderGraph.graphData;
             var pixelGraphEvalFunction = new ShaderStringBuilder();
             var activeNodeList = ListPool<AbstractMaterialNode>.Get();
-
+            NodeUtils.DepthFirstCollectNodesFromNode(activeNodeList, ((AbstractMaterialNode)graph.outputNode), NodeUtils.IncludeSelf.Include, Enumerable.Range(1, 31).ToList()); // hardcoded hd pixel slots.
 
             string pixelGraphInputStructName = "SurfaceDescriptionInputs";
             string pixelGraphOutputStructName = "SurfaceDescription";
@@ -1580,7 +1668,7 @@ namespace UnityEditor.ShaderGraph
             var sharedProperties = new PropertyCollector();
             var pixelRequirements = ShaderGraphRequirements.FromNodes(activeNodeList, ShaderStageCapability.Fragment, false);
 
-            NodeUtils.DepthFirstCollectNodesFromNode(activeNodeList, ((AbstractMaterialNode)graph.outputNode), NodeUtils.IncludeSelf.Include, Enumerable.Range(1,31).ToList()); // hardcoded hd pixel slots.
+            
 
             GraphUtil.GenerateSurfaceDescriptionFunction(
                 activeNodeList,
