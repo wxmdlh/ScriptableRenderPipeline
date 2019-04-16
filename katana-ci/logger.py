f29501ad43d92f405da13673febfd1f403062203
sophia <sophia@unity3d.com>
2019-04-16 15:49:00 +0200

    Create logger.py

---
+++ katana-ci/logger.py
@@ -0,0 +1,10 @@
+import datetime
+import os
+
+log_file_name = 'log.txt'
+
+def log(message):
+    global log_file_name
+    print(message)
+    with open(log_file_name, 'a+') as f:
+        f.write('[{0}] {1}\n'.format(datetime.datetime.now(), message))
