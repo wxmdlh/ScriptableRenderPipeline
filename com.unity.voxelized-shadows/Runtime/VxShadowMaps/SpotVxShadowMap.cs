0c419694f3d03504b6d9c84c25c164885d66d055
Seongdae Kim <37893618+Seongdae@users.noreply.github.com>
2019-04-17 17:17:04 +0900

    Add register and unregister point/spot vxsm to manager

--- com.unity.voxelized-shadows/Runtime/VxShadowMaps/SpotVxShadowMap.cs
+++ com.unity.voxelized-shadows/Runtime/VxShadowMaps/SpotVxShadowMap.cs
@@ -9,6 +9,15 @@ namespace UnityEngine.Experimental.VoxelizedShadows
         public override int voxelResolutionInt => (int)VoxelResolution._4096;
         public override VoxelResolution subtreeResolution => VoxelResolution._4096;
 
+        private void OnEnable()
+        {
+            VxShadowMapsManager.instance.RegisterVxShadowMapComponent(this);
+        }
+        private void OnDisable()
+        {
+            VxShadowMapsManager.instance.UnregisterVxShadowMapComponent(this);
+        }
+
         public override bool IsValid()
         {
             return false;
