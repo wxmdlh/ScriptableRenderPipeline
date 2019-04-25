d6ef78b727dc1183bf49dcd2b851633d63d07ad0
Seongdae Kim <seongdae@unity3d.com>
2019-04-25 16:51:59 +0900

    Add inspector editor to dir vxsm and vxsm container, Control index of vxsm

---
+++ com.unity.voxelized-shadows/Editor/VxShadowMaps/DirectionalVxShadowMapEditor.cs
@@ -0,0 +1,58 @@
+﻿using UnityEditor;
+
+namespace UnityEngine.Experimental.VoxelizedShadows
+{
+    [CustomEditor(typeof(DirectionalVxShadowMap))]
+    public class DirectionalVxShadowMapEditor : Editor
+    {
+        private void OnEnable()
+        {
+
+        }
+
+        public override void OnInspectorGUI()
+        {
+            var vxsm = target as DirectionalVxShadowMap;
+
+            var shadowsBlendOptions = new string[]
+            {
+                ShadowsBlendMode.OnlyVxShadows.ToString(),
+                ShadowsBlendMode.BlendDynamicShadows.ToString(),
+            };
+
+            EditorGUILayout.Space();
+            var result0 = EditorGUILayout.Popup("Shadows Blend", (int)vxsm.ShadowsBlend, shadowsBlendOptions);
+            vxsm.ShadowsBlend = (ShadowsBlendMode)result0;
+
+            EditorGUILayout.FloatField("Volume Scale", vxsm.volumeScale);
+
+            bool validVxsm = vxsm.index >= 0 && vxsm.index < vxsm.DataList.Count;
+            if (validVxsm)
+            {
+                float indexFloat = (float)vxsm.index;
+                float maxIndexFloat = (float)vxsm.DataList.Count - 1;
+
+                float sizeInMBytes = (float)vxsm.DataList[vxsm.index].SizeInBytes / (1024.0f * 1024.0f);
+                EditorGUILayout.FloatField("Size(MB)", sizeInMBytes);
+
+                string indexString = "Index(" + vxsm.DataList.Count + ")";
+                var result1 = EditorGUILayout.Slider(indexString, indexFloat, 0.0f, maxIndexFloat);
+                vxsm.index = (int)result1;
+            }
+        }
+
+        [DrawGizmo(GizmoType.Selected|GizmoType.Active)]
+        static void DrawGizmosSelected(DirectionalVxShadowMap vxsm, GizmoType gizmoType)
+        {
+            var light = vxsm.GetComponent<Light>();
+            var volumeScale = vxsm.volumeScale;
+
+            if (light != null)
+            {
+                Gizmos.color = new Color(1.0f, 0.0f, 0.0f, 0.3f);
+                Gizmos.matrix = light.transform.localToWorldMatrix;
+                Gizmos.DrawCube(Vector3.zero, new Vector3(volumeScale, volumeScale, volumeScale));
+            }
+        }
+    }
+}
--- com.unity.voxelized-shadows/Runtime/VxShadowMaps/DirectionalVxShadowMap.cs
+++ com.unity.voxelized-shadows/Runtime/VxShadowMaps/DirectionalVxShadowMap.cs
@@ -12,8 +12,8 @@ namespace UnityEngine.Experimental.VoxelizedShadows
 
         private int _index = 0;
         private uint _beginOffset =>
-            VxShadowsDataList.Count > _index ?
-            VxShadowsDataList[_index].BeginOffset : 0;
+            DataList.Count > _index ?
+            DataList[_index].BeginOffset : 0;
 
         public override int index
         {
@@ -23,11 +23,21 @@ namespace UnityEngine.Experimental.VoxelizedShadows
             }
             set
             {
-                _index = value;
+                if (_index == value)
+                    return;
 
-                var vxsData = VxShadowsDataList[_index];
-                gameObject.transform.position = vxsData.Position;
-                gameObject.transform.rotation = vxsData.Rotation;
+                int maxCount = Mathf.Max(0, DataList.Count - 1);
+                _index = Mathf.Clamp(value, 0, maxCount);
+
+                if (_index < DataList.Count)
+                {
+                    var vxsData = DataList[_index];
+                    gameObject.transform.position = vxsData.Position;
+                    gameObject.transform.rotation = vxsData.Rotation;
+
+                    // todo : remove this later
+                    Debug.Log("BeginOffset: " + DataList[_index].BeginOffset);
+                }
             }
         }
         public override uint bitset
@@ -39,8 +49,14 @@ namespace UnityEngine.Experimental.VoxelizedShadows
                 uint bitBlendMode = isOnlyVxsm ? (uint)0x80000000 : (uint)0x40000000;
                 uint bitBeginOffset = _beginOffset & 0x3FFFFFFF;
 
-                return IsValid() ? (bitBlendMode | bitBeginOffset) : bitBeginOffset;
+                return IsValid() ? (bitBlendMode | bitBeginOffset) : 0;
             }
+        }
+
+        public override void ResetData()
+        {
+            _index = -1;
+            DataList.Clear();
         }
 
         private void OnEnable()
@@ -59,19 +75,5 @@ namespace UnityEngine.Experimental.VoxelizedShadows
                 VxShadowMapsManager.Instance.Container != null &&
                 enabled;
         }
-
-#if UNITY_EDITOR
-        public void OnDrawGizmos()
-        {
-            var light = GetComponent<Light>();
-
-            if (light != null)
-            {
-                Gizmos.color = new Color(1.0f, 0.0f, 0.0f, 0.3f);
-                Gizmos.matrix = light.transform.localToWorldMatrix;
-                Gizmos.DrawCube(Vector3.zero, new Vector3(volumeScale, volumeScale, volumeScale));
-            }
-        }
-#endif
     }
 }
