5733b90492a350755114e1fc02b1d0d8f8db96e0
Seongdae Kim <seongdae@unity3d.com>
2019-04-25 16:51:59 +0900

    Add inspector editor to dir vxsm and vxsm container, Control index of vxsm

--- com.unity.voxelized-shadows/Editor/VxShadowMaps/VxShadowMapsContainerEditor.cs
+++ com.unity.voxelized-shadows/Editor/VxShadowMaps/VxShadowMapsContainerEditor.cs
@@ -5,5 +5,24 @@ namespace UnityEngine.Experimental.VoxelizedShadows
     [CustomEditor(typeof(VxShadowMapsContainer))]
     public class VxShadowMapsContainerEditor : Editor
     {
+        public override void OnInspectorGUI()
+        {
+            var container = target as VxShadowMapsContainer;
+
+            float sizeInBytes = container.Resources.VxShadowMapList.Length * sizeof(uint);
+            float sizeInMBytes = sizeInBytes / (1024.0f * 1024.0f);
+            EditorGUILayout.LabelField("Total Size(MB): " + sizeInMBytes);
+
+            foreach (var vxsData in container.Resources.VxShadowsDataList)
+            {
+                sizeInMBytes = (float)vxsData.SizeInBytes / (1024.0f * 1024.0f);
+
+                string vxsmInfo0 = "Type: " + vxsData.Type.ToString();
+                string vxsmInfo1 = "BeginOffset: " + vxsData.BeginOffset;
+                string vxsmInfo2 = "Size(MB): " + sizeInMBytes;
+
+                EditorGUILayout.LabelField(vxsmInfo0 + ", " + vxsmInfo1 + ", " + vxsmInfo2);
+            }
+        }
     }
 }
