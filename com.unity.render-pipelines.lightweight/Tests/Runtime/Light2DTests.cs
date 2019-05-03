f047a2db76d78c08bb8c8c01daa8d5694eb3fd60
Yuanxing Cai <yuanxing@unity3d.com>
2019-05-03 16:56:06 +0800

    Light culling tests.

--- com.unity.render-pipelines.lightweight/Tests/Runtime/Light2DTests.cs
+++ com.unity.render-pipelines.lightweight/Tests/Runtime/Light2DTests.cs
@@ -6,16 +6,18 @@ using UnityEngine.TestTools;
 namespace UnityEngine.Rendering.LWRP.Tests
 {
     [TestFixture]
-    class SingleLight2DTests
+    class SingleObjectLight2DTests
     {
         Light2DManager m_LightManager;
         GameObject m_TestObject;
+        Light2D m_TestLight;
 
         [SetUp]
         public void Setup()
         {
             m_LightManager = new Light2DManager();
             m_TestObject = new GameObject("Test Object");
+            m_TestLight = m_TestObject.AddComponent<Light2D>();
         }
 
         [TearDown]
@@ -28,32 +30,28 @@ namespace UnityEngine.Rendering.LWRP.Tests
         [Test]
         public void SetupCullingSetsBoundingSpheresAndCullingIndices()
         {
-            var light = m_TestObject.AddComponent<Light2D>();
-
             Light2D.SetupCulling(Camera.main);
 
             Assert.NotNull(Light2DManager.boundingSpheres);
             Assert.AreEqual(1024, Light2DManager.boundingSpheres.Length);
-            Assert.AreEqual(0, light.lightCullingIndex);
+            Assert.AreEqual(0, m_TestLight.lightCullingIndex);
         }
 
         [UnityTest]
         public IEnumerator ChangingBlendStyleMovesTheLightToTheCorrectListInLightManager()
         {
-            var light = m_TestObject.AddComponent<Light2D>();
-
-            light.blendStyleIndex = 1;
+            m_TestLight.blendStyleIndex = 1;
 
             // LightManager update happens in LateUpdate(). So we must test the result in the next frame.
             yield return null;
 
             Assert.AreEqual(0, Light2DManager.lights[0].Count);
-            Assert.AreSame(light, Light2DManager.lights[1][0]);
+            Assert.AreSame(m_TestLight, Light2DManager.lights[1][0]);
         }
     }
 
     [TestFixture]
-    class MultipleLight2DTests
+    class MultipleObjectLight2DTests
     {
         Light2DManager m_LightManager;
         GameObject m_TestObject1;
@@ -96,6 +94,32 @@ namespace UnityEngine.Rendering.LWRP.Tests
             Assert.AreSame(light3, Light2DManager.lights[0][0]);
             Assert.AreSame(light1, Light2DManager.lights[0][1]);
             Assert.AreSame(light2, Light2DManager.lights[0][2]);
+        }
+
+        [UnityTest]
+        public IEnumerator IsLightVisibleReturnsTrueIfInCameraView()
+        {
+            var camera = m_TestObject1.AddComponent<Camera>();
+            var light = m_TestObject2.AddComponent<Light2D>();
+            light.transform.position = camera.transform.position;
+            Light2D.SetupCulling(camera);
+
+            yield return null;
+
+            Assert.IsTrue(light.IsLightVisible(camera));
+        }
+
+        [UnityTest]
+        public IEnumerator IsLightVisibleReturnsFalseIfNotInCameraView()
+        {
+            var camera = m_TestObject1.AddComponent<Camera>();
+            var light = m_TestObject2.AddComponent<Light2D>();
+            light.transform.position = camera.transform.position + new Vector3(9999.0f, 0.0f, 0.0f);
+            Light2D.SetupCulling(camera);
+
+            yield return null;
+
+            Assert.IsFalse(light.IsLightVisible(camera));
         }
     }
 }
